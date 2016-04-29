6574d010bc4b76e680366bcb268616a7da808923
t2013anurag <tiwari.anurag126@gmail.com>
2016-04-29 21:01:46 +0530

    Code Derangements

--- Permutations/count-derangements.cpp
+++ Permutations/count-derangements.cpp
@@ -1,1 +1,27 @@
-countDer(n) = (n-1)*[countDer(n-1) + countDer(n-2)]
+#include<bits/stdc++.h>
+
+// Time complexity O(n) and space complexity O(n);
+using namespace std;
+
+int countDer(int n) 
+{
+	int der[n+1];
+	//Base cases
+	der[0] = 1;
+	der[1] = 0;
+	der[2] = 1;
+
+	for(int i= 3; i<= n; i++)
+		der[i] = (i-1)*(der[i-1] + der[i-2]);
+
+	return der[n];
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter a number \n";
+	cin>>n;
+	cout<<"The number of derangements are "<<countDer(n)<<endl;
+	return 0;
+}
